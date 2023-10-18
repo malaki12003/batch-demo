69ca2fbe4b95370887d51e0de2f79c71d6013551
Mahdi Malaki <malaki12003@gmail.com>
2023-10-18 16:48:45 -0400

    Add EDI reader example

---
+++ src/main/java/com/helixz/spring/batch/demo/Main.java
@@ -0,0 +1,101 @@
+package com.helixz.spring.batch.demo;/*
+	Milyn - Copyright (C) 2006 - 2010
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License (version 2.1) as published by the Free Software
+	Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU Lesser General Public License for more details:
+	http://www.gnu.org/licenses/lgpl.txt
+*/
+
+
+import com.helixz.spring.batch.demo.model.Order;
+import org.milyn.Smooks;
+import org.milyn.SmooksException;
+import org.milyn.container.ExecutionContext;
+import org.milyn.event.report.HtmlReportGenerator;
+import org.milyn.io.StreamUtils;
+import org.milyn.payload.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+
+/**
+ * Simple example main class.
+ *
+ * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
+ */
+public class Main {
+
+    private static byte[] messageIn = readInputMessage();
+
+    private final Smooks smooks;
+
+    protected Main() throws IOException, SAXException {
+        // Instantiate Smooks with the config...
+        smooks = new Smooks("smooks-config.xml");
+    }
+
+    protected JavaResult runSmooksTransform(ExecutionContext executionContext) throws IOException, SAXException, SmooksException {
+        try {
+            JavaResult javaResult = new JavaResult();
+
+            // Configure the execution context to generate a report...
+            executionContext.setEventListener(new HtmlReportGenerator("target/report/report.html"));
+
+            // Filter the input message to the outputWriter, using the execution context...
+            smooks.filterSource(executionContext, new StreamSource(new ByteArrayInputStream(messageIn)), javaResult);
+
+            return javaResult;
+        } finally {
+            smooks.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, SAXException, SmooksException {
+        System.out.println("\n\n==============Message In==============");
+        System.out.println(new String(messageIn));
+        System.out.println("======================================\n");
+
+        Main smooksMain = new Main();
+        ExecutionContext executionContext = smooksMain.smooks.createExecutionContext();
+        org.milyn.payload.JavaResult result = smooksMain.runSmooksTransform(executionContext);
+
+
+        System.out.println("\n==============EDI as Java Object Graph=============");
+        Order order = (Order) result.getBean("order");
+        order.getHeader().getCustomer().setLastName("Malaki");
+        System.out.println(order);
+    }
+
+    private static byte[] readInputMessage() {
+        try {
+            return StreamUtils.readStream(new FileInputStream("input-message.edi"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "<no-message/>".getBytes();
+        }
+    }
+
+    private static void pause(String message) {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("> " + message);
+            in.readLine();
+        } catch (IOException e) {
+        }
+        System.out.println("\n");
+    }
+
+    public JavaResult runSmooksTransform() throws IOException, SAXException {
+        ExecutionContext executionContext = smooks.createExecutionContext();
+        return runSmooksTransform(executionContext);
+    }
+}
